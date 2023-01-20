ef8dedde98ee8c0e351947610d6403cfb559069a
Brenden Sprague <brenden@Brendens-MBP-2.lan>
2023-01-20 01:40:13 -0800

    Added Delete and search by job

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -40,23 +40,80 @@ const users = {
     ]
  }
 
- app.get('/users/:id', (req, res) => {
-    const id = req.params['id']; //or req.params.id
-    let result = findUserById(id);
-    if (result === undefined || result.length == 0)
-        res.status(404).send('Resource not found.');
-    else {
-        result = {users_list: result};
-        res.send(result);
-    }
+app.get('/users', (req, res) => {
+   const name = req.query.name;
+   const job = req.query.job;
+   if (name != undefined){
+       let result = findUserByName(name);
+       result = {users_list: result};
+       res.send(result);
+   }
+   if (job != undefined){
+      let result = findUserByJob(job);
+      result = {users_list: result};
+      res.send(result);
+  }
+   else{
+        res.send(users);
+   }
+});
+
+const findUserByName = (name) => { 
+   return users['users_list'].filter( (user) => user['name'] === name); 
+}
+
+const findUserByJob = (job) => { 
+   return users['users_list'].filter( (user) => user['job'] === job); 
+}
+
+app.get('/users/:id', (req, res) => {
+   const id = req.params['id']; //or req.params.id
+   let result = findUserById(id);
+   if (result === undefined || result.length == 0)
+       res.status(404).send('Resource not found.');
+   else {
+       result = {users_list: result};
+       res.send(result);
+   }
 });
 
 function findUserById(id) {
-    return users['users_list'].find( (user) => user['id'] === id); // or line below
-    //return users['users_list'].filter( (user) => user['id'] === id);
+   return users['users_list'].find( (user) => user['id'] === id); // or line below
+   //return users['users_list'].filter( (user) => user['id'] === id);
 }
 
-const findUserByName = (name) => { 
-    return users['users_list'].filter( (user) => user['name'] === name); 
+app.post('/users', (req, res) => {
+   const userToAdd = req.body;
+   addUser(userToAdd);
+   fetch('http://localhost:5000/users');
+   res.status(200).end();
+});
+
+function addUser(user){
+   users['users_list'].push(user);
 }
+
+
+/*   
+http://localhost:5000/users/abc123   
+{
+   id : 'abc123', 
+}
+*/
+app.delete('/users/:id', (req, res) => {
+   const id = req.params['id'];
+   deleteUser(id);
+   res.status(200).end();
+});
+
+function deleteUser(id){
+   const index = users['users_list'].findIndex(user => user.id === id);
+   if (index !== -1) {
+      users['users_list'].splice(index, 1);
+   }
+}
+
+
+
+
 
